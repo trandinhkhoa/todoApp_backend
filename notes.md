0c3b1daff15c772bbf1446a6e488d1db6fc2bde9
Khoa TRAN <khoatrandinh94@gmail.com>
2022-08-15 22:45:29 +0200

    declare endpoints

---
+++ notes.md
@@ -0,0 +1,12 @@
+
+HTTP request: where to put params ?
+ query param
+ GET /cars?color=blue
+ route param (as part of the url ?
+ GET /cars/:id
+ in payload ?
+https://stackoverflow.com/questions/25385559/rest-api-best-practices-args-in-query-string-vs-in-request-body
+https://stackoverflow.com/questions/4024271/rest-api-best-practices-where-to-put-parameters
+https://stackoverflow.com/questions/30967822/when-do-i-use-path-params-vs-query-params-in-a-restful-api
+
+When to use PUT ? when to use POST ?
--- server.js
+++ server.js
@@ -5,6 +5,46 @@ app.get("/", (req,res) => {
   res.send("HELLO WORLD");
 })
 
+// CREATE
+app.post("/createTask", (req,res) => {
+  console.log(req.method, req.url);
+  res.send("task created");
+})
+
+// READ
+app.get("/getAllTasks", (req,res) => {
+  console.log(req.method, req.url);
+  res.send("all tasks returned");
+})
+
+// localhost:3000/getTask/abc123
+app.get("/getTask/:taskId", (req,res) => {
+  console.log(req.method, req.url, req.params.taskId);
+  let response = "Task " + req.params.taskId + " returned";
+  res.send(response);
+})
+
+// UPDATE
+app.post("/updateTask", (req,res) => {
+  console.log(req.method, req.url);
+  res.send("task udpated");
+})
+
+// DELETE
+app.delete("/deleteAllTasks", (req,res) => {
+  console.log(req.method, req.url);
+  res.send("all tasks deleted");
+})
+
+// could have used route param like GET does.
+// but here, for demonstrative purpose, I use query param
+// localhost:3000/deleteTask?taskId=abc123
+app.delete("/deleteTask", (req,res) => {
+  console.log(req.method, req.url, req.params.taskId, req.query);
+  let response = "Task " + req.params.taskId + " deleted";
+  res.send(response);
+})
+
 const PORT = 3000;
 
 app.listen(PORT, ()=> {
